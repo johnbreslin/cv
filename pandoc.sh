43266fc6e0affb267bf8977ec5c6c816a468a7b3
John Breslin <john.breslin@nuigalway.ie>
2023-01-24 13:14:56 +0000

    Modified pandoc.sh to generate separate publications HTML file

--- pandoc.sh
+++ pandoc.sh
@@ -1,1 +1,4 @@
-pandoc -o Curriculum_Vitae.html Curriculum_Vitae.tex
+sed 's/centerline/underline/g' Curriculum_Vitae.tex > Curriculum_Vitae_temp.tex
+pandoc Curriculum_Vitae_temp.tex -o Curriculum_Vitae.html
+pandoc publications.tex -o publications.html --bibliography=publications.bib --citeproc --csl=publications.csl
+rm Curriculum_Vitae_temp.tex
